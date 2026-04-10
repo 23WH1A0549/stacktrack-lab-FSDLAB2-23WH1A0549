1bb138180aab3e7ffb2fbed70474494fc658736e
23wh1a0549 <23wh1a0549@bvrithyderabad.edu.in>
2026-04-10 21:20:54 +0530

    Setup API client for fetching tasks

--- src/api/client.js
+++ src/api/client.js
@@ -1,2 +1,8 @@
 // TODO: Set the base URL for your API
 
+const BASE_URL = "http://localhost:3000";
+
+export const fetchTasks = async (query = "") => {
+  const res = await fetch(`${BASE_URL}/tasks?query=${query}`);
+  return res.json();
+};
